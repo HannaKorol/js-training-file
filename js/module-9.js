8aa1dbf17c4be3e1fcec1b695412cb8bce0965f0
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-07-19 17:24:31 +0200

    commit module 10

--- js/module-9.js
+++ js/module-9.js
@@ -124,6 +124,11 @@ console.log(sessionStorage); // Storage {length: 0}
 
 
 
+//Інструменти веброзробки
+//*Vite (https://vitejs.dev/) (французьке слово, що означає «швидкий», вимовляється [vit], як «veet») — це інструмент збірки проєкту для розробки вебдодатків на базі JavaScript.
+//*Node.js — легке та ефективне середовище виконання JavaScript поза браузером
+
+
 
 
 
@@ -316,3 +321,71 @@ function handleFormSubmit(event) {
   - Отримуємо значення зі сховища
  - Якщо там щось було, оновлюємо DOM
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------
+//Домашнє завдання по модулю 9
+
+ 
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const textarea = form.querySelector("textarea");
+
+populateTextArea (); // викликаєсо функцію для отримання значення зі сховища
+
+textarea.addEventListener("input", handleTextAreaInput)
+
+const formData = {
+    email: "", 
+    message: "",
+};
+
+/*
+ * - Скасовуємо стандартну поведінку
+ * - Видаляємо повідомлення зі сховища
+ * - Очищуємо форму
+ */
+
+function handleFormSubmit(event){
+    event.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset();
+}
+
+
+
+/*
+ * - Отримуємо значення поля
+ * - Зберігаємо його у сховище
+ */
+
+function handleTextAreaInput(event){
+const value = event.target.value;
+localStorage.setItem(STORAGE_KEY, value)
+};
+
+
+/*
+ * - Отримуємо значення зі сховища
+ * - Якщо там щось було, оновлюємо DOM
+ */
+
+function populateTextArea () {
+ const savedMassage = localStorage.getItem(STORAGE_KEY);
+
+if(savedMassage){
+    textarea.value = savedMassage;
+}
+} 
